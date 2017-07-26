34f6cb4817472fd422d723489b145484dd94e923
mattza <matknu@kth.se>
2017-07-26 07:34:38 +0000

    Det är en början

--- createQuestion.js
+++ createQuestion.js
@@ -1,53 +1,43 @@
+'use strict'
 const fetch = require('node-fetch');
+const cheerio = require('cheerio')
 
 let getRandomPageLink = () => `https://sv.wikipedia.org/wiki/Special:Slumpsida`;
 
-// let url = getRandomPageLink();
-// let host = url.split('/').slice(0, 3).join('/');
-// console.log('host', host);
-// fetch(getRandomPageLink(), {
-//   host: "proxyw.ppm.nu",
-//   port: 8080,
-//   path: url,
-//   headers: { host }
-// })
-//   .then(function (res) {
-//     return res.text();
-//   }).then(function (body) {
-//     console.log(body);
-//   }, function (e) {
-//     console.log('error', e);
-//   });
-console.log('isPM', process.argv.some(arg => arg === '--pm'));
-let getData = url => new Promise((res, rej) => {
-  require('https').get(process.argv.some(arg => arg === '--pm') ? {
-    host: "proxyw.ppm.nu",
-    port: 8080,
-    path: url,
-    headers: { host: url.split('/').slice(0, 3).join('/') }
-  } : url,
-    response => {
-      let datas = '';
-      console.log(Object.keys(response));
-      console.log(response.statusCode);
-      response.on('data', data => {
-        console.log('?', data);
-        datas += data.toString();
-      });
-      response.on('end', () => {
-        try {
-          console.log('datas', datas)
-          res(datas);
-        } catch (error) {
-          rej(error);
-        }
-      })
-      response.on('error', error => {
-        console.error(error);
-      })
-    })
-})
 
-getData(getRandomPageLink()).then(data => {
-  console.log(data);
-})
+let getAllIndexes = (arr, val) => {
+    var indexes = [], i = -1;
+    while ((i = arr.indexOf(val, i+1)) != -1){
+        indexes.push(i);
+    }
+    return indexes;
+}
+
+let getBody = url => fetch(url)
+  .then(res =>res.text());
+
+getBody('https://sv.wikipedia.org/wiki/Lessebo').then(body => {
+  let $body = cheerio.load(body);
+  console.log($body('#firstHeading').html())
+  let content = $body('#bodyContent').text();
+  let years = Array(500).fill(0).map((v,i) => 1500+i);
+  let noChars = 50
+  
+  console.log(
+    years.filter(year => !!~content.indexOf(year))
+    .reduce((acc,year) => acc.concat(getAllIndexes(content,year).map(index => ({index,year}))),[])
+    .reduce((acc,obj) => {
+      let item = acc.find(item => item.year === obj.year);
+      if(!item){
+        item = {year: obj.year,index:[]}
+        acc.push(item);
+      }
+      item.index.push(obj.index);
+      return acc;
+    },[])
+    .filter(item => item.index.length >1)
+    .sort((a,b) => a.index.length<b.index.length)
+    .slice(0,3)
+    .map(item => ({year:item.year, texts: item.index.map(index => content.slice(index-noChars,index+noChars))}))
+  )
+});
