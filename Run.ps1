498b1b44dbeb2a572814f1d421f6c6e68ec424c8
Fredrik Löwenhamn <lowet84@gmail.com>
2017-10-20 17:26:07 +0200

    Updated run script

--- Run.ps1
+++ Run.ps1
@@ -13,36 +13,22 @@ if (!(Test-Path rethinkdb\rethinkdb.exe))
     Move-Item rethinkdb\rethinkdb-$rethinkDbVersion\rethinkdb.exe rethinkdb\rethinkdb.exe
 }
 
-Remove-Job -Force when.RethinkDb
-Remove-Job -Force when.ApiServer
-Remove-Job -Force when.Web
 $location = Get-Location
 $rethinkDbJob = {
-  Set-Location $args[0]
   Set-Location rethinkdb
   ./rethinkdb.exe
 }
 $apiJob = {
-  Set-Location $args[0]
   Set-Location when.ApiServer
   dotnet restore
   dotnet watch run
 }
 $webJob = {
-  Set-Location $args[0]
   Set-Location when.Web
   npm install
   npm run dev
 }
 
-Start-Job -name when.RethinkDb -ArgumentList $location -ScriptBlock $rethinkDbJob
-Start-Job -name when.ApiServer -ArgumentList $location -ScriptBlock $apiJob
-Start-Job -name when.Web -ArgumentList $location -ScriptBlock $webJob
-
-Write-Host "Press any key to exit ..."
-$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-Write-Host "Killing processes ..."
-Remove-Job -Force when.RethinkDb
-Remove-Job -Force when.ApiServer
-Remove-Job -Force when.Web
-Exit-PSSession
+Invoke-Expression "cmd /c start powershell -Command { $rethinkDbJob }"
+Invoke-Expression "cmd /c start powershell -Command { $apiJob }"
+Invoke-Expression "cmd /c start powershell -Command { $webJob }"
