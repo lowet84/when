a22b4667b0ebcde05337fcafbda811829f5833c3
Fredrik Löwenhamn <lowet84@gmail.com>
2018-01-08 14:05:07 +0100

    Changing default questions

--- when.ApiServer/Program.cs
+++ when.ApiServer/Program.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Reflection;
 using System.Threading.Tasks;
 using GraphQlRethinkDbLibrary;
 using HtmlAgilityPack;
 using Microsoft.AspNetCore.Hosting;
+using Newtonsoft.Json;
 using when.ApiServer.Model;
 
 namespace when.ApiServer
@@ -34,22 +36,19 @@ namespace when.ApiServer
         {
             var context = new UserContext();
             var existing = context.GetAll<Question>(UserContext.ReadType.Shallow);
-            var parallel = 10;
-            var samplesPerYear = 10;
+            //var parallel = 10;
+            //var samplesPerYear = 10;
             if (existing.Length == 0)
             {
                 var questions = new List<Question>();
-                var years = Enumerable.Range(1900, 118).ToList();
-                while (years.Count > 0)
+                var json = Resource.Questions;
+                var data = JsonConvert.DeserializeObject<QuestionData[]>(json);
+
+                foreach (var item in data)
                 {
-                    var current = years.Take(parallel).ToList();
-                    var tasks = current.Select(d => new Task<List<Question>>(() => GetQuestions(d, samplesPerYear))).ToList();
-                    tasks.ForEach(d => d.Start());
-                    Task.WaitAll(tasks.ToArray());
-                    var results = tasks.Select(d => d.Result).SelectMany(d => d).ToList();
-                    current.ForEach(d => years.Remove(d));
-                    questions.AddRange(results);
+                    questions.Add(new Question(item.question, item.year));
                 }
+                
                 for (int index = 0; index < questions.Count; index++)
                 {
                     var question = questions[index];
@@ -59,33 +58,12 @@ namespace when.ApiServer
             }
         }
 
-        public static List<Question> GetQuestions(int year, int samples)
+        private class QuestionData
         {
-            var ret = new List<Question>();
-            var random = new Random();
-            using (var client = new HttpClient())
-            {
-                var context = new UserContext();
-                Console.WriteLine($"Seeding: {year}");
-                var html = client.GetStringAsync($"http://www.onthisday.com/events/date/{year}").Result;
-                var doc = new HtmlDocument();
-                doc.LoadHtml(html);
-                var texts = doc.DocumentNode
-                    .Descendants("li")
-                    .Where(d =>
-                        d.Attributes.Contains("class")
-                        &&
-                        d.Attributes["class"].Value.Contains("event-list__item")
-                    ).Select(d => d.LastChild.InnerText).ToList();
-
-                foreach (var text in texts.OrderBy(x => random.Next()).Take(samples))
-                {
-                    var question = new Question(text, year);
-                    ret.Add(question);
-                }
-            }
-
-            return ret;
+            public string question { get; set; }
+            public string category { get; set; }
+            public string extraInfo { get; set; }
+            public int year { get; set; }
         }
     }
 }
